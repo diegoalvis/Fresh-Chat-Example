827bcd5cd2878e27d598feaefb606a73eb76207c
Diego Alvis <diegoalvispal@gmail.com>
2020-07-13 14:06:41 -0500

    Set user info for the chat

--- app/src/main/java/com/diegoalvis/freshchattest/FreshChatApp.kt
+++ app/src/main/java/com/diegoalvis/freshchattest/FreshChatApp.kt
@@ -1,10 +1,20 @@
 package com.diegoalvis.freshchattest
 
 import android.app.Application
+import com.freshchat.consumer.sdk.Freshchat
+
+import com.freshchat.consumer.sdk.FreshchatConfig
+
 
 class FreshChatApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val config = FreshchatConfig(BuildConfig.freshChatAppId, BuildConfig.freshChatAppKey)
+        config.isCameraCaptureEnabled = true
+        config.isGallerySelectionEnabled = true
+        config.isResponseExpectationEnabled = true
+        Freshchat.getInstance(applicationContext).init(config)
     }
 }
--- app/src/main/java/com/diegoalvis/freshchattest/MainActivity.kt
+++ app/src/main/java/com/diegoalvis/freshchattest/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.diegoalvis.freshchattest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.freshchat.consumer.sdk.Freshchat
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the user object for the current installation
+        val freshchatUser = Freshchat.getInstance(applicationContext).user.apply {
+            firstName = "Pelican"
+            lastName = "Test"
+            email = "diego@pelican.com"
+        }
+        // Call setUser so that the user information is synced with Freshchat's servers
+        Freshchat.getInstance(applicationContext).user = freshchatUser
+
+        button.setOnClickListener { Freshchat.showConversations(applicationContext) }
+
     }
 }
